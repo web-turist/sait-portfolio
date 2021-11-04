6a9aaebb18ea2cb2af3ff81344b2c24a18b2c6a8
Maxim Khorkhordin <x.maxim84@mail.ru>
2021-11-04 16:18:43 +0300

    завершил меню и написал скрипт

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6*/
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
