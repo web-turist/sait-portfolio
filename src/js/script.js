2987eadf6dde3b7fce5926785f1f5dfcac3d4245
Maxim Khorkhordin <x.maxim84@mail.ru>
2021-11-10 17:59:34 +0300

    В блок про навыки добавлены шкалы и прописан скрипт

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,10 @@ close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
